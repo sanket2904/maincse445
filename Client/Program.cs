31564f166be2cea4927560b084e3193a413e8cb8
Sanket Agarwal <agarwalsanket29@gmail.com>
2023-02-08 19:46:45 -0700

    resolving error

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using cse445;
+string num = Environment.GetCommandLineArgs()[0];  // read the argument from the command line
+
+if (num == "1") {
+    
+}
